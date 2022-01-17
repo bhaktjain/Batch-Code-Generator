be9ff2846d2958803f65a9acfd6a201c0ef1183b
bhaktjain <bhaktjain501@gmail.com>
2022-01-17 18:35:12 +0530

    PORT

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 const {db} = require('./dbs/model')
 const {app} = require('./server')
 
-
+const PORT = process.env.PORT || 4588
 const start = async() =>{
 
     try{
@@ -9,7 +9,7 @@ const start = async() =>{
         await db.sync()
 
         app.listen(4588,()=>{
-            console.log('server started on http://localhost:4588')
+            console.log(`server started on http://localhost:${PORT}`)
         })
     }
     catch(err){
